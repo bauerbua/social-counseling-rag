061368fb530c0e8021007be93b24bfd56ca9b46e
jakob.bauer <jakobbauer@thesiestagroup.com>
2025-02-01 01:40:36 +0100

    improved indexing - pdfminer instead of pypdf - split document by sentence

--- db.py
+++ db.py
@@ -7,14 +7,18 @@ from haystack.components.preprocessors import DocumentSplitter, DocumentCleaner
 from haystack.components.routers import FileTypeRouter
 from haystack.components.joiners import DocumentJoiner
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
-from haystack.components.converters import PyPDFToDocument
+from haystack.components.converters import PDFMinerToDocument
 from haystack import Pipeline
 from haystack.document_stores.types import DuplicatePolicy
-
+from haystack import tracing
+from haystack.tracing.logging_tracer import LoggingTracer
 import logging
 
 logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
-logging.getLogger("haystack").setLevel(logging.INFO)
+logging.getLogger("haystack").setLevel(logging.DEBUG)
+
+tracing.tracer.is_content_tracing_enabled = True # to enable tracing/logging content (inputs/outputs)
+tracing.enable_tracing(LoggingTracer(tags_color_strings={"haystack.component.input": "\x1b[1;31m", "haystack.component.name": "\x1b[1;34m"}))
 
 """
 This script initializes a Weaviate client and a WeaviateDocumentStore to store and manage documents.
@@ -33,37 +37,35 @@ The preprocessing steps include:
 The script can be executed directly to load and index all PDFs in a specified directory.
 """
 
-# Connect to WeaviateDocumentStore
-
-# Load the PDF file and create embeddings
 def load_and_preprocess_files(directory_path: str):
     document_store = WeaviateDocumentStore(url='http://localhost:8080')
-
     file_type_router = FileTypeRouter(mime_types=["application/pdf"]) # If other file types are present, add them here
-    pdf_converter = PyPDFToDocument()
+    pdf_converter = PDFMinerToDocument()
     document_joiner = DocumentJoiner()
     document_cleaner = DocumentCleaner()
-    document_splitter = DocumentSplitter(split_by="word", split_length=150)
+    document_splitter = DocumentSplitter(split_by="period", split_length=200, split_overlap=50)
     document_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
     document_embedder.warm_up()
-    document_writer = DocumentWriter(document_store=document_store, duplicate_policy=DuplicatePolicy.OVERWRITE)
+    document_writer = DocumentWriter(document_store=document_store, policy=DuplicatePolicy.OVERWRITE)
     preprocessing_pipeline = Pipeline()
+    
     preprocessing_pipeline.add_component(instance=file_type_router, name="file_type_router")
-    preprocessing_pipeline.add_component(instance=pdf_converter, name="pypdf_converter")
+    preprocessing_pipeline.add_component(instance=pdf_converter, name="pdfminer_converter")
     preprocessing_pipeline.add_component(instance=document_joiner, name="document_joiner")
     preprocessing_pipeline.add_component(instance=document_cleaner, name="document_cleaner")
     preprocessing_pipeline.add_component(instance=document_splitter, name="document_splitter")
     preprocessing_pipeline.add_component(instance=document_embedder, name="document_embedder")
     preprocessing_pipeline.add_component(instance=document_writer, name="document_writer")
 
-    preprocessing_pipeline.connect("file_type_router.application/pdf", "pypdf_converter.sources")
-    preprocessing_pipeline.connect("pypdf_converter", "document_joiner")
+    preprocessing_pipeline.connect("file_type_router.application/pdf", "pdfminer_converter.sources")
+    preprocessing_pipeline.connect("pdfminer_converter", "document_joiner")
     preprocessing_pipeline.connect("document_joiner", "document_cleaner")
     preprocessing_pipeline.connect("document_cleaner", "document_splitter")
     preprocessing_pipeline.connect("document_splitter", "document_embedder")
     preprocessing_pipeline.connect("document_embedder", "document_writer")
 
-    preprocessing_pipeline.run({"file_type_router": {"sources": list(Path(directory_path).glob("**/*"))}})
+    result = preprocessing_pipeline.run({"file_type_router": {"sources": list(Path(directory_path).glob("**/*"))}})
+    print(result)
     document_store.client.close()
 
 
